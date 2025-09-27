432f55ac4a53e1eb0058ac42deda17fa79ae2469
Thiago <100661398+thiagosabreu@users.noreply.github.com>
2025-09-26 21:02:26 -0300

    fix (vtk-10): Alterado tipo de radarId e adicionado apiurl no .env

--- app/services/radar-api.ts
+++ app/services/radar-api.ts
@@ -1,7 +1,7 @@
 import type { Radar } from '~/types/radar';
 
 export async function fetchRadars(): Promise<Radar[]> {
-  const response = await fetch('/radars');
+  const response = await fetch(`${process.env.API_URL}/radares`);
   if (!response.ok) throw new Error('Erro ao buscar radares');
   return await response.json();
 }
--- app/types/radar.ts
+++ app/types/radar.ts
@@ -1,5 +1,5 @@
 export interface Radar {
-  radarId: number;
+  radarId: string;
   regiaoId: number;
   latitude: number;
   longitude: number;
