00580057e8553571834a00feb97c9ffcd391c9bf
BeatrizPlacido <biaplacsilva0212@gmail.com>
2025-10-19 10:37:42 -0300

    feat: refactor Map component to use GeoJSON for radar markers and improve image loading

--- app/components/Map.vue
+++ app/components/Map.vue
@@ -5,7 +5,8 @@
 </template>
 
 <script setup lang="ts">
-import { Map, MapStyle, Marker, config as mapConfig } from '@maptiler/sdk';
+import { Map, MapStyle, Popup, config as mapConfig } from '@maptiler/sdk';
+import type { FeatureCollection, Point, GeoJsonProperties } from 'geojson';
 import { shallowRef, onMounted, onUnmounted, markRaw } from 'vue';
 import '@maptiler/sdk/dist/maptiler-sdk.css';
 import { fetchRadars } from '~/services/radar-api';
@@ -20,30 +21,70 @@ onMounted(async () => {
 
   const initialState = { lng: -45.85477630787629, lat: -23.21252050854134, zoom: 12 };
 
-  map.value = markRaw(new Map({
-    container: mapContainer.value!,
-    style: MapStyle.STREETS,
-    center: [initialState.lng, initialState.lat],
-    zoom: initialState.zoom
-  }));
+  map.value = markRaw(
+    new Map({
+      container: mapContainer.value!,
+      style: MapStyle.STREETS,
+      center: [initialState.lng, initialState.lat],
+      zoom: initialState.zoom
+    })
+  )
 
-  // Busca os radares e adiciona os marcadores
-  const radars = await fetchRadars();
-  addRadarMarkers(radars);
-});
+  map.value.on('load', async () => {
+    await map.value!.loadImage('/images/radar-icon.png')
+      .then(img => {
+        if (!map.value!.hasImage('radar-icon')) {
+          map.value!.addImage('radar-icon', img.data)
+        }
+      })
+      .catch(err => console.error('Erro ao carregar imagem:', err))
+
+    const radars = await fetchRadars()
+    addRadarLayer(radars)
+  })
+})
 
 onUnmounted(() => {
   map.value?.remove();
 });
 
-// Função para adicionar marcadores de radar
-function addRadarMarkers(radars: Radar[]) {
-  if (!map.value) return;
-  radars.forEach(radar => {
-    new Marker({ color: "#113E63" })
-      .setLngLat([radar.longitude, radar.latitude])
-      .addTo(map.value!);
-  });
+function addRadarLayer(radars: Radar[]) {
+  if (!map.value) return
+
+  const geojson: FeatureCollection<Point, GeoJsonProperties> = {
+    type: 'FeatureCollection',
+    features: radars.map(radar => ({
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [radar.longitude, radar.latitude]
+      },
+      properties: {
+        id: radar.radarId,
+        type: 'radar'
+      }
+    }))
+  };
+
+  if (!map.value.getSource('radars')) {
+    map.value.addSource('radars', {
+      type: 'geojson',
+      data: geojson
+    })
+  }
+
+  if (!map.value.getLayer('radars-layer')) {
+    map.value.addLayer({
+      id: 'radars-layer',
+      type: 'symbol',
+      source: 'radars',
+      layout: {
+        'icon-image': 'radar-icon',
+        'icon-size': 1,
+        'icon-allow-overlap': true
+      }
+    })
+  }
 }
 </script>
 
