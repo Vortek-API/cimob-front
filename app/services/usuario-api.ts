65dc335a3f0d1311725fac4ac81ce39699d0ba77
Thiago <100661398+thiagosabreu@users.noreply.github.com>
2025-10-25 12:08:47 -0300

    feat (vtk-91): Adicionado carregamento dos usuários de acordo com o backend

--- app/services/usuario-api.ts
+++ app/services/usuario-api.ts
@@ -1,9 +1,21 @@
 import { getApi } from "~/config/axios-config";
+import type { Usuario } from "~/types/usuario";
+
 export async function getCargo(email: string): Promise<string> {
   // O endpoint é /cargo/{email} e faz parte da API principal (sem prefixo /auth)
   const api = getApi(); 
+  
   console.log("Buscando cargo para o email:", email);
+  
   const { data } = await api.get(`usuario/cargo/${email}`);
+  
   return data;
-
 }
+
+export async function getUsuarios(): Promise<Usuario[]>{
+  const api = getApi();
+  const response = await api.get(`/usuario`);
+  
+  return response.data;
+}
+
